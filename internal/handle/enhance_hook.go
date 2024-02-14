33cc1fad084f8bf70d05b602c04abcc997408f6b
IllTamer <78360471+IllTamer@users.noreply.github.com>
2024-02-14 17:44:08 +0800

    feat: support to set_client_name api

--- internal/handle/enhance_hook.go
+++ internal/handle/enhance_hook.go
@@ -1,6 +1,8 @@
 package handle
 
 import (
+	"errors"
+	"fmt"
 	global "github.com/IUnlimit/perpetua/internal"
 	"github.com/IUnlimit/perpetua/internal/utils"
 	"github.com/bytedance/gopkg/util/gopool"
@@ -12,15 +14,24 @@ import (
 // enhance onebot API impl
 
 var mx sync.Mutex
-var hookMap = map[string]func(global.MsgData) (global.MsgData, error){
-	"set_restart": setRestartHook,
+var hookMap = map[string]func(global.MsgData, *Handler) (global.MsgData, error){
+	"set_restart":     setRestartHook,
+	"set_client_name": setClientName,
 }
+var emptyParams = make(map[string]interface{}, 0)
 
 // TryTouchEnhanceHook check and handle api if exists
 // @return data, exist, error
-func TryTouchEnhanceHook(msgData global.MsgData) (global.MsgData, bool, error) {
+func TryTouchEnhanceHook(msgData global.MsgData, handler *Handler) (global.MsgData, bool, error) {
+	params := msgData["params"]
+	if _, ok := params.(map[string]interface{}); !ok {
+		if params != nil {
+			return nil, false, errors.New(fmt.Sprintf("unknown params field type: %s", params))
+		}
+		msgData["params"] = emptyParams
+	}
 	if hook := hookMap[msgData["action"].(string)]; hookMap != nil {
-		data, err := hook(msgData)
+		data, err := hook(msgData, handler)
 		if err != nil {
 			return nil, true, err
 		}
@@ -30,8 +41,8 @@ func TryTouchEnhanceHook(msgData global.MsgData) (global.MsgData, bool, error) {
 }
 
 // reboot onebot instance
-func setRestartHook(msgData global.MsgData) (global.MsgData, error) {
-	delay := msgData["delay"]
+func setRestartHook(msgData global.MsgData, _ *Handler) (global.MsgData, error) {
+	delay := msgData["params"].(map[string]interface{})["delay"]
 	if _, ok := delay.(int); ok {
 		// sleep before locking so that other threads that end delay earlier can seize the lock
 		time.Sleep(time.Duration(delay.(int)) * time.Millisecond)
@@ -53,4 +64,15 @@ func setRestartHook(msgData global.MsgData) (global.MsgData, error) {
 	})
 
 	return utils.BuildWSGoodResponse("async", msgData["echo"].(string)), nil
+}
+
+// set the ws connect name
+func setClientName(msgData global.MsgData, handler *Handler) (global.MsgData, error) {
+	name := msgData["params"].(map[string]interface{})["name"]
+	if _, ok := name.(string); !ok {
+		return utils.BuildWSBadResponse(fmt.Sprintf("empty or unsupport name: %s", name), msgData["echo"].(string)), nil
+	}
+
+	handler.name = name.(string)
+	return utils.BuildWSGoodResponse("ok", msgData["echo"].(string)), nil
 }
