bc4d848917bcba2643bcd153f844a06d79a09ce6
IllTamer <78360471+IllTamer@users.noreply.github.com>
2024-02-17 00:35:21 +0800

    fix: wrong handler invoke when dispatch

--- internal/handle/websocket.go
+++ internal/handle/websocket.go
@@ -288,8 +288,8 @@ func readFromNTQQLoop(handle *Handler, conn *websocket.Conn) error {
 		}
 		// when closed, staying dispatch
 		for _, v := range receivers {
+			handler := v.(*Handler)
 			gopool.Go(func() {
-				handler := v.(*Handler)
 				handler.AddMessage(uuid)
 				handler.Receive <- true
 			})
